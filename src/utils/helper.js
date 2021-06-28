4a3252f61709ab6ec1536d01eb63eda5784121c4
Ayush Bansal <ayu023ban@gmail.com>
2021-06-28 12:12:09 +0530

    Add Polar Chart and Make Dark theme version for apex charts and improve performance

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,28 @@
 export function download(content, fileName, contentType) {
-    var a = document.createElement("a");
-    var file = new Blob([content], { type: contentType });
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
+  var a = document.createElement("a");
+  var file = new Blob([content], { type: contentType });
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
+
+export function getCommon(arr1, arr2) {
+  arr1.sort();
+  arr2.sort();
+  var common = [];
+  var i = 0,
+    j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] == arr2[j]) {
+      common.push(arr1[i]);
+      i++;
+      j++;
+    } else if (arr1[i] < arr2[j]) {
+      i++;
+    } else {
+      j++;
+    }
   }
+
+  return common;
+}
--- src/views/apex_demo.js
+++ src/views/apex_demo.js
@@ -1,73 +1,100 @@
-// import React from "react";
+import React from "react";
 import Chart from "react-apexcharts";
 import json_data from "../assets/opssat_analysis.json";
 import columns from "../assets/opssat_columns.json";
+import { getCommon } from "../utils/helper";
 
 const timestamps = json_data.data.timestamps.map((time) => {
   let temp = new Date(time);
   return temp.getTime();
 });
-
-let columns_to_ignore = [
-  // "number_of_tx_bytes_since_reboot",
-  // "total_rx_packets",
-  // "total_tx_bytes",
-  // "total_rx_bytes",
-  // "Proton 1 MeV",
-  // "Electron 2 MeV",
-  // "Electron 800 KeV",
-  // "last_valid_packet_timestamp",
+const data = [
+  {
+    name: "positive",
+    data: [],
+  },
+  { name: "negative", data: [] },
 ];
-
-const data = [];
-columns.forEach((col) => {
-  if (columns_to_ignore.findIndex((el) => el === col) === -1) {
-    let temp = {};
-    temp.name = col;
-    temp.data = [];
-    for (let i = 0; i < timestamps.length; i++) {
-      const temp2 = {};
-      temp2.x = timestamps[i];
-      temp2.y = json_data.data.values[col]["individual_values"][i].toFixed(2);
-      temp.data.push(temp2);
+for (let i = 0; i < timestamps.length; i++) {
+  let pos = 0;
+  let neg = 0;
+  columns.forEach((col) => {
+    let temp = json_data.data.values[col]["individual_values"][i];
+    if (temp > 0) {
+      pos += temp;
+    } else {
+      neg += temp;
     }
-    data.push(temp);
+  });
+  data[0].data.push({ x: timestamps[i], y: pos.toFixed(2) });
+  data[1].data.push({ x: timestamps[i], y: neg.toFixed(2) });
+}
+
+const events = json_data.data.events.map(
+  (evt) => json_data.data.timestamps[evt]
+);
+const eventData = {};
+columns.forEach((col) => {
+  let common = getCommon(
+    events,
+    json_data.data.values[col]["individual_events_detected"]
+  );
+  if (common.length > 0) {
+    eventData[col] = common.length;
   }
 });
-
+console.log(eventData);
 const xAxisAnnotations = json_data.data.events.map((event) => {
   return {
     x: timestamps[event],
     strokeDashArray: 0,
-    borderColor: "#775DD0",
+    borderColor: "#FF0080",
   };
 });
 
 const options = {
   chart: {
-    stacked: true,
     height: 280,
     type: "area",
+    foreColor: "#ccc",
     zoom: {
       autoScaleYaxis: true,
     },
+    toolbar: {
+      autoSelected: "pan",
+      show: false,
+    },
+  },
+  colors: ["#00BAEC"],
+  stroke: {
+    width: 3,
+  },
+  grid: {
+    borderColor: "#555",
+    clipMarkers: false,
+    yaxis: {
+      lines: {
+        show: false,
+      },
+    },
+  },
+  tooltip: {
+    theme: "dark",
   },
   dataLabels: {
     enabled: false,
   },
-  legend: {
-    show: false,
-  },
   fill: {
     type: "gradient",
     gradient: {
-      shadeIntensity: 1,
-      opacityFrom: 0.7,
-      opacityTo: 0.9,
-      stops: [0, 90, 100],
+      enabled: true,
+      opacityFrom: 0,
+      opacityTo: 1,
     },
   },
-  // tooltip: false,
+  legend: {
+    show: false,
+  },
   xaxis: {
     type: "datetime",
   },
@@ -76,20 +103,72 @@ const options = {
   },
 };
 
+const eventOptions = {
+  chart: {
+    type: "polarArea",
+    foreColor: "#ccc",
+  },
+  labels: Object.keys(eventData),
+  stroke: {
+    width: 1,
+    colors: undefined,
+  },
+  fill: {
+    opacity: 1,
+  },
+  plotOptions: {
+    polarArea: {
+      rings: {
+        strokeWidth: 0,
+      },
+      spokes: {
+        strokeWidth: 0,
+      },
+    },
+  },
+  yaxis: { show: false },
+  legend: {
+    position: "bottom",
+  },
+};
+
 const App = () => {
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "spaceEvenly",
         alignItems: "center",
-        height: "100vh",
+        background: "#000524",
+        color: "white",
+        flexDirection: "column",
       }}
     >
       <div>
         <h2>Opssat1-short analysis with normalized values</h2>
         <div className="mixed-chart">
           <Chart options={options} series={data} type="area" width="1000" />
+        </div>
+      </div>
+      <div
+        style={{
+          marginTop: "10rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "3rem",
+        }}
+      >
+        <div>
+          <h2>Contributions of telemetry in events</h2>
+          <div className="mixed-chart">
+            <Chart
+              options={eventOptions}
+              series={Object.values(eventData)}
+              type="polarArea"
+              width={600}
+            />
+          </div>
         </div>
       </div>
     </div>
